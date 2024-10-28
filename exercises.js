a52c1d451ed4d4145e1256d4dc5931f262e58200
XOKEESH <hello@kishamartin.com>
2024-10-28 06:53:53 -0700

    problem 3 solved

--- exercises.js
+++ exercises.js
@@ -1,6 +1,6 @@
 // Use `.map()` to iterate over the following array:
 
-// const nums = [13, 87, 2, 89, 12, 4, 90, 63]
+const nums = [13, 87, 2, 89, 12, 4, 90, 63]
 
 // Create a new array where each value is multiplied by 2 and log the new array.
 
@@ -12,9 +12,9 @@
 
 // console.log(multipliedNums)
 
-// const petsArray = ['Rover', 'Snuffles', 'Toto', 'Spot'];
+const petsArray = ['Rover', 'Snuffles', 'Toto', 'Spot'];
 
-// const [firstPet, secondPet] = petsArray;
+const [firstPet, secondPet] = petsArray;
 
 // console.log(firstPet); // 'Rover'
 // console.log(secondPet); // 'Snuffles'
@@ -27,12 +27,45 @@
 
 const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies']
 
-// Your code here
+// // Your code here
 
 const [firstTopping, secondTopping] = pizzaToppings
 
-console.log(firstTopping)
-console.log(secondTopping)
+// console.log(firstTopping)
+// console.log(secondTopping)
 
-console.log(pizzaToppings[0])
-console.log(pizzaToppings[1])
+// console.log(pizzaToppings[0])
+// console.log(pizzaToppings[1])
+
+const person = {
+    name: 'Alex',
+    role: 'Software Engineer',
+  }
+  
+  // Object destructuring:
+  const { name, role } = person;
+  
+//   console.log(name) // 'Alex'
+//   console.log(role) // 'Software Engineer'
+  
+  // Equivalent in traditional dot notation:
+//   console.log(person.name) // 'Alex'
+//   console.log(person.role) // 'Software Engineer'
+  
+  // Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  }
+  
+  // Your code here
+  
+const { make, model } = car
+
+// console.log(make)
+// console.log(model)
+
+// console.log(car.make)
+// console.log(car.model)
+
