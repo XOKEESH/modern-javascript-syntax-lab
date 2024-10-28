9730c22baa609cf51dad19b44dd5c341e2808aa3
XOKEESH <hello@kishamartin.com>
2024-10-28 13:24:58 -0700

    solved #10

--- exercises.js
+++ exercises.js
@@ -129,10 +129,73 @@
 //     friends: 24,
 // }
 
-const propertyName = 'blocked'
+// const propertyName = 'blocked'
 
-const blockedUsers = {
-    [propertyName]: 4,
-}
+// const blockedUsers = {
+//     [propertyName]: 4,
+// }
 
-console.log(blockedUsers)
+// console.log(blockedUsers)
+
+// export const myNumber = 123;
+// export const myString = 'Hello';
+
+// export default function superCoolFunction() {
+//     /* ... */
+//   }
+
+// //   import { myNumber, myString } from './myData.js';
+// //   import superCoolFunction from './superCoolFunction.js';
+
+//   import * as MyData from './myData.js';
+//     // console.log(MyData.myNumber);
+//     // console.log(MyData.myString);
+
+//     function addThreeNumbers(numA, numB = 2, numC = 1) {
+//         return numA + numB + numC;
+//       }
+      
+//       addThreeNumbers(2);
+
+
+
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+
+// Convert the following `if...else` statement in to a ternary:
+
+// if (pizza === 'tasty') {
+//   console.log('yum')
+// } else {
+//   console.log('yuck')
+// }
+
+// Your code here
+
+// let pizza = 'tasty'
+// console.log(pizza === 'tasty' ? 'yum' : 'yuck')
+
+const localLangConfig = null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+const LANG = localLangConfig || 'en';
+
+// Log the result
+console.log('Language setting:', LANG);
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+const USER_THEME = userSavedTheme || 'light';
+
+// Log the result
+console.log('User theme setting:', USER_THEME);
---
+++ importingFile.js
@@ -0,0 +1,5 @@
+// import name from './exportingFIle.js';
+// console.log(name);
+
+import name, { age, job } from './exportingFIle.js';
+console.log(name, age, job);
